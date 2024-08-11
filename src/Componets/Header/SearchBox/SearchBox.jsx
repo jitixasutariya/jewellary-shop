f97daee26edc3338c69a155824d5da9621139819
jitixa <jitixa.kothiya@envertis.com.au>
2024-08-11 10:03:44 +1000

    update search box changes

--- src/Componets/Header/SearchBox/SearchBox.jsx
+++ src/Componets/Header/SearchBox/SearchBox.jsx
@@ -1,22 +1,42 @@
 // src/SearchBox.js
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faSearch,
   faMicrophone,
   faCamera,
-} from "@fortawesome/free-solid-svg-icons"; // Import the new icon
-
+} from "@fortawesome/free-solid-svg-icons";
 import "./SearchBox.css"; // Import the CSS file for styling
 
 const SearchBox = () => {
+  const [isMegaMenuOpen, setMegaMenuOpen] = useState(false);
+
+  const toggleMegaMenu = () => {
+    setMegaMenuOpen(!isMegaMenuOpen);
+  };
+
   return (
     <div className="search-container">
-      <FontAwesomeIcon icon={faMicrophone} className="microphone-icon" />{" "}
-      {/* New icon */}
-      <input type="text" className="search-input" placeholder="Search..." />
+      <FontAwesomeIcon
+        icon={faMicrophone}
+        className="microphone-icon"
+        onClick={toggleMegaMenu}
+      />
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search..."
+        onClick={toggleMegaMenu}
+      />
       <FontAwesomeIcon icon={faCamera} className="camera-icon" />
       <FontAwesomeIcon icon={faSearch} className="search-icon" />
+
+      {isMegaMenuOpen && (
+        <div className="mega-menu">
+          {/* Your Mega Menu content goes here */}
+          <p>Mega Menu Content</p>
+        </div>
+      )}
     </div>
   );
 };
